413b50fb735e6760c625e589067b2a5262ebcf65
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-09-05 18:16:29 +0200

    - Adding recommend hotels listview

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -9,65 +9,91 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          headerWidget(),
-          SizedBox(height: 50),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text("Popular", style: TextStyle(fontWeight: FontWeight.w900)),
-                Text("See all", style: TextStyle(color: Colors.pink, fontWeight: FontWeight.w700)),
-              ],
-            ),
-          ),
-          SizedBox(height: 15),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height / 4.25,
-              child: ListView.separated(
-                separatorBuilder: (context, index) => SizedBox(width: 15),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return listViewItemPopular(index);
-                },
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            headerWidget(),
+            SizedBox(height: 50),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text("Popular", style: TextStyle(fontWeight: FontWeight.w900)),
+                  Text("See all", style: TextStyle(color: Colors.pink, fontWeight: FontWeight.w700)),
+                ],
               ),
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                Text("Categories", style: TextStyle(fontWeight: FontWeight.w900)),
-              ],
-            ),
-          ),
-          SizedBox(height: 15,),
-          Padding(
-            padding: const EdgeInsets.only(left: 20),
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height / 12,
-              child: ListView.separated(
-                separatorBuilder: (context, index) => SizedBox(width: 15),
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return listViewItemCategories(index);
-                },
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height / 4.25,
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => SizedBox(width: 15),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return listViewItemPopular(index);
+                  },
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text("Categories", style: TextStyle(fontWeight: FontWeight.w900)),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height / 12,
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => SizedBox(width: 15),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return listViewItemCategories(index);
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Text("Recommend", style: TextStyle(fontWeight: FontWeight.w900)),
+                ],
+              ),
+            ),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height / 6,
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => SizedBox(width: 15),
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return listViewItemRecommend(index);
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -151,6 +177,7 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
   listViewItemCategories(int index) {
     return Container(
       width: MediaQuery.of(context).size.width / 2.25,
@@ -166,9 +193,51 @@ class _HomePageState extends State<HomePage> {
         children: <Widget>[
           Text(
             "Activity",
-            style: TextStyle(letterSpacing: 1.5,color: Colors.white, fontWeight: FontWeight.w500),
+            style: TextStyle(letterSpacing: 1.5, color: Colors.white, fontWeight: FontWeight.w500),
           ),
         ],
+      ),
+    );
+  }
+
+  listViewItemRecommend(int index) {
+    return Container(
+      width: MediaQuery.of(context).size.width / 1.65,
+      height: MediaQuery.of(context).size.height / 6,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          image: DecorationImage(
+              image: NetworkImage(
+                  "https://content.r9cdn.net/rimg/himg/e0/47/1b/hotelsdotcom-774079552-93a44910_w-775528.jpg?crop=true&width=500&height=350"),
+              fit: BoxFit.fill)),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  "50\$",
+                  style: TextStyle(
+                      letterSpacing: 2.0, fontSize: 16, color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Your Location",
+                  style: TextStyle(fontSize: 11,color: Colors.white, fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+            Icon(
+              Icons.arrow_forward,
+              color: Colors.white,
+              size: 24,
+            ),
+          ],
+        ),
       ),
     );
   }
